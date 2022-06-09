9f729f4d25cf052b539dbf12604ec5b30d98483e
0zstriker <ozstriker9@hotmail.com>
2022-06-09 12:44:25 +1000

    Removed

--- kali-post-install.zsh
+++ kali-post-install.zsh
@@ -51,13 +51,6 @@ echo "####  Gnu Debugger (GDB) Installed  ####"
 sudo apt install -y -qq snmp-mibs-downloader
 echo "####  SNMP Tools Installed  ####"
 
-## Typora
-#sudo wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-#sudo apt install -y -qq software-properties-common
-#sudo add-apt-repository 'deb https://typora.io/linux ./'
-#sudo apt update -y -qq
-#sudo apt install -y -qq typora
-#echo "####  Typora Installed  ####"
 
 ## AutoRecon
 pip3 install --quiet git+https://github.com/Tib3rius/AutoRecon.git
